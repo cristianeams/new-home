546290afcf95c613206efd452070a0ab5fc3d3bb
Cristiane Silva <cristiane@neon.cc>
2023-09-27 15:30:16 -0700

    Add opentag image

--- components/SEO.js
+++ components/SEO.js
@@ -6,9 +6,11 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
-      <meta property="og:image:type" content="<generated>" />
-      <meta property="og:image:width" content="<generated>" />
-      <meta property="og:image:height" content="<generated>" />
+      <meta property="og:url" content="https://cristiane.dev/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Cristiane Silva - Frontend Developer" />
+      <meta property="og:description" content="Cristiane Silva - I'm a Frontend developer based in Vancouver.  I specialize in building accessible and user-friendly products. My expertise includes technologies like TypeScript and React, allowing me to create interactive and responsive experiences for the web." />
+      <meta property="og:image" content="https://cristiane.dev/opentag.png" />
     </Head>
-  );
+
 }
