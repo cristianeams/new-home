baa836f91d7266d0bead34bda92e2f7597c5ac0e
Cristiane Silva <cristiane@neon.cc>
2023-09-27 15:18:12 -0700

    Fix: typos

--- utils/global-data.js
+++ utils/global-data.js
@@ -8,7 +8,7 @@ export const getGlobalData = () => {
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : `${new Date().getFullYear()}`;
-  const titleSEO = `I&apos;m  a Frontend developer based in Vancouver.  I specialize in building accessible and user-friendly products. My expertise includes technologies like TypeScript and React, allowing me to create interactive and responsive experiences for the web.`
+  const titleSEO = `I'm  a Frontend developer based in Vancouver.  I specialize in building accessible and user-friendly products. My expertise includes technologies like TypeScript and React, allowing me to create interactive and responsive experiences for the web.`
   const neonDescription = `I use React, TypeScript, and other technologies to build impactful products and collaborate with others to build reusable components and keep everyone in sync with the UI. I participate in Scrum practices, present demos, and strive to deliver high-quality products that meet users' needs while staying up-to-date with the latest technologies and best practices.`;
   const starlingDescription = `I built the front end of the Mental Health Platform using Cordova and Vue.js, creating reusable components and writing clean, scalable code. I collaborated with the back-end team and contributed to documentation to maintain high coding standards.`
   const randomDescription = `
