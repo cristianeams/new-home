6c0a62285c06fbce4d7146cf56a9ca94b7156cf1
Cristiane Silva <cristiane@neon.cc>
2024-04-12 14:55:41 -0700

    Add experience in bullet points

--- components/Experience.js
+++ components/Experience.js
@@ -1,58 +1,77 @@
-import Link from 'next/link';
-
 export default function Experience({ descriptions }) {
-    const { neonDescription, starlingDescription, randomDescription } = descriptions
-    return (
+  const { neonDescription, starlingDescription, randomDescription } =
+    descriptions;
+  return (
+    <div className="px-3 mb-48">
+      <h4 className="text-xl font-semibold lg:text-4xl mb-6">Experience</h4>
+      <a
+        className="group "
+        href={`https://neon.cc/`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h4 className="text-sm lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
+          Neon - Frontend Developer
+        </h4>
 
-        <div className="px-3 mb-48" >
-            <h4 className="text-xl font-semibold lg:text-4xl mb-6">
-                Experience
-            </h4>
-            <a
-                className="group "
-                href={`https://neon.cc/`}
-                target="_blank" rel="noreferrer"
-            >
-                <h4 className="text-sm lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
-                    Neon - Frontend Developer
-                </h4>
+        <p className="text-sm lg:text-md mb-4">
+          September 2019 - September 2023
+        </p>
+        <ul className="list-disc list-inside">
+          {neonDescription.map((task) => (
+            <li className="text-sm lg:text-base pb-2" key={task}>
+              {task}
+            </li>
+          ))}
+        </ul>
+      </a>
+      <a
+        className="group"
+        href={`https://www.starlingminds.com/`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h4 className="text-sm mt-8 lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
+          Starling Minds - Frontend Developer
+        </h4>
+      </a>
 
-                <p className="text-sm lg:text-md mb-4">September 2019 - September 2023</p>
-                <p className="text-sm lg:text-base">{neonDescription}</p>
+      <p className="text-sm lg:text-md mb-4">September 2018 - July 2019</p>
+      <ul className="list-disc list-inside">
+        {starlingDescription.map((task) => (
+          <li className="text-sm lg:text-base pb-2" key={task}>
+            {task}
+          </li>
+        ))}
+      </ul>
+      <a
+        className="group"
+        href={`https://www.behance.net/cristianeams`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <h4 className="text-sm mt-8 lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
+          UI Designer - Self Employed
+        </h4>
+      </a>
 
-            </a>
-            <a
-                className="group"
-                href={`https://www.starlingminds.com/`}
-                target="_blank" rel="noreferrer"
-            >
-                <h4 className="text-sm mt-8 lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
-                    Starling Minds - Frontend Developer
-                </h4>
-            </a>
-
-            <p className="text-sm lg:text-md mb-4">September 2018 - July 2019</p>
-            <p className="text-sm lg:text-base">{starlingDescription}</p>
-            <a
-                className="group"
-                href={`https://www.behance.net/cristianeams`}
-                target="_blank" rel="noreferrer"
-            >
-                <h4 className="text-sm mt-8 lg:text-xl group-hover:underline decoration-4 decoration-violet-300 mb-1">
-                    UI Designer - Self Employed
-                </h4>
-            </a>
-
-            <p className="text-sm lg:text-md mb-4">January 2014 - June 2017</p>
-            <p className="text-sm lg:text-base">{randomDescription}</p>
-            <button
-                type="button"
-                aria-label="Download resume as PDF"
-                className="flex h-full bg-violet-300 mt-8  rounded-3xl py-2 px-4 dh-10 hover:drop-shadow-xl"
-            >
-                <a href="/Resume_CristianeS.pdf" download>Download PDF</a>
-
-            </button>
-        </div>
-    );
+      <p className="text-sm lg:text-md mb-4">January 2014 - June 2017</p>
+      <ul className="list-disc list-inside">
+        {randomDescription.map((task) => (
+          <li className="text-sm lg:text-base pb-2" key={task}>
+            {task}
+          </li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        aria-label="Download resume as PDF"
+        className="flex h-full bg-violet-300 mt-8  rounded-3xl py-2 px-4 dh-10 hover:drop-shadow-xl"
+      >
+        <a href="/Resume_CristianeS.pdf" download>
+          Download PDF
+        </a>
+      </button>
+    </div>
+  );
 }
--- utils/global-data.js
+++ utils/global-data.js
@@ -8,20 +8,27 @@ export const getGlobalData = () => {
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : `${new Date().getFullYear()}`;
-  const titleSEO = `Skilled Front End Web Developer with 5+ years of experience in JavaScript/TypeScript, HTML/CSS, using React, NextJS and GraphQL. Excellent problem-solving and communication skills working with B2B companies.`
-  const neonDescription = `Led the front-end team in developing a B2B talent acquisition platform that included an admin tool with user-based permissions and a job board to promote hires based on referrals.
-  Built a design system using Chakra UI, Storybook React, Next.js and TypeScript to speed up our ability to prototype, iterate and communicate through development and design.
-  Created a comprehensive onboarding documentation and guide to help newly hired developers get started.
-  Presented weekly demo to our partners, addressing technical concerns and proposing solutions while working collectively to incorporate feedback.
-  Participated in code reviews, feature planning, and design reviews.
-  Investigated and resolved technical issues related to front-end development.`
-  const starlingDescription = `Built responsive, and accessible web components for a B2B mental health training platform designed for educators.
-  Implemented user authentication and API integrations.
-  Developed and maintained automated tests to ensure the quality of the code base.
-  Participated in code reviews, feature planning, and design reviews.`
-  const randomDescription = `
-  Created user-friendly interfaces for various web and mobile applications, ensuring a positive user experience.
-  Conducted user research to discover user needs and to inform design decisions.`
+  const titleSEO = `Front End Developer with 5+ years of experience in JavaScript/TypeScript, HTML/CSS, using React, NextJS and GraphQL. Excellent problem-solving and communication skills working with B2B companies.`;
+
+  const neonDescription = [
+    `Developed and implemented Neon's Design System, Cornerstone, using TypeScript, React, and Storybook, resulting in a 20% improvement in code reusability and a more consistent UI/UX across the platform.`,
+    'Led the front-end development of Alltourage, a talent acquisition dashboard platform that streamlines the job application process.',
+    'Prioritized tasks based on impact, improving project efficiency and reducing time-to-market, boosting overall productivity.',
+    'Collaborated with cross-functional teams to ensure seamless integration and communication between application components, enabling successful feature launches in a fast-paced environment with multiple stakeholders',
+    'Investigated and resolved technical issues related to front - end development.',
+  ];
+  const starlingDescription = [
+    'Built responsive, and accessible components for a mental health training platform designed for educators',
+    'Implemented user authentication and API integrations.',
+    'Developed and maintained automated tests to ensure the quality of the code base.',
+    'Participated in code reviews, feature planning, and design reviews.',
+  ];
+
+  const randomDescription = [
+    'Created user-friendly interfaces for various web and mobile applications, ensuring a positive user experience.',
+    'Conducted user research to discover user needs and to inform design decisions.',
+  ];
+
   return {
     name,
     titleSEO,
@@ -29,9 +36,6 @@ export const getGlobalData = () => {
     footerText,
     neonDescription,
     starlingDescription,
-    randomDescription
+    randomDescription,
   };
 };
-
-
-
